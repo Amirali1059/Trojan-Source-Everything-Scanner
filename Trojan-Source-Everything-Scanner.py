4c8f473a53b1b700a46a749978417b24cc50ae17
Amirali1059 <66733532+Amirali1059@users.noreply.github.com>
2021-11-19 21:31:39 +0330

    Update Trojan-Source-Everything-Scanner.py

--- Trojan-Source-Everything-Scanner.py
+++ Trojan-Source-Everything-Scanner.py
@@ -46,27 +46,27 @@ available_drives = [
 # scan all drives
 scan_ext = tuple(scan_extentions)
 malware_files = []
+all_files = []
 for drive in available_drives:
     print(f"Discovering drive {drive} ...", end=" ")
-    all_files = []
     for root, dirs, files in os.walk(drive+"\\"):
         print("|/-\\"[(int(time() % 1*4))], end="\b")
         all_files += [root+"\\"+file for file in files if file.lower().endswith(scan_ext)]
     print("Done!")
     print(f"found {len(all_files)} scanable files in drive {drive} ...")
-    print(f"Scannig drive {drive} ...", end=" ")
-    for file in all_files:
-        print("|/-\\"[(int(time() % 1*4))], end="\b")
-        try:
-            raw_code = open(file, "r", encoding="utf-8").read()
-        except (UnicodeDecodeError, FileNotFoundError, PermissionError):
-            continue
-        for char in malware_chars:
-            if char in raw_code:
-                count = raw_code.count(char)
-                chrs = "char" if count == 1 else "chars"
-                malware_files.append(f"found {count} \\u{ord(char):x} {chrs} in \"{file}\"!")
-    print("Done!")
+print(f"Scannig all founded files ...", end=" ")
+for file in all_files:
+    print("|/-\\"[(int(time() % 1*4))], end="\b")
+    try:
+        raw_code = open(file, "r", encoding="utf-8").read()
+    except (UnicodeDecodeError, FileNotFoundError, PermissionError):
+        continue
+    for char in malware_chars:
+        if char in raw_code:
+            count = raw_code.count(char)
+            chrs = "char" if count == 1 else "chars"
+            malware_files.append(f"found {count} \\u{ord(char):x} {chrs} in \"{file}\"!")
+print("Done!")
 print("\n    Scan Results:\n")
 print("\n".join(malware_files))
 print()
